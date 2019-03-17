6cec35ce344c88f40746bf0c6c43cf92f2711593
Maximilian Goisser <goisser94@gmail.com>
2019-03-17 23:33:35 +0100

    feat(cli): switch skip-http for check-http flag
    CLOSES #42

---
+++ tests/working_http_check/src/lib.rs
@@ -0,0 +1,15 @@
+/// Foo function
+///
+/// Has something to do with [some website](http://example.com).
+pub fn foo() {}
+
+/// Bar function
+pub fn bar() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
