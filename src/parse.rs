01423519a1bb62e35882cdc837a02b96c24a68ca
Joshua Nelson <jyn514@gmail.com>
2020-10-30 16:26:30 -0400

    Switch from rcdom to lol_html
    This is both more efficient and easier to read. It does introduce a ton
    of dependencies, but fortunately compile times are about the same as
    before.

--- src/parse.rs
+++ src/parse.rs
@@ -1,50 +1,38 @@
 use std::collections::HashSet;
 use std::path::Path;
 
-use html5ever::parse_document;
-use html5ever::rcdom::{Handle, NodeData, RcDom};
-use html5ever::tendril::TendrilSink;
 use log::{debug, info};
+use lol_html::{element, RewriteStrSettings};
 use url::Url;
 
 /// Parse the html file at the provided path and check the availablility of all links in it.
 pub fn parse_html_file(root_dir: &Path, path: &Path) -> HashSet<Url> {
     info!("Checking doc page at {}", path.display());
-    let dom = parse_document(RcDom::default(), Default::default())
-        .from_utf8()
-        .from_file(path)
-        .unwrap();
+    let html = std::fs::read_to_string(path)
+        .unwrap_or_else(|e| panic!("{} did not contain valid UTF8: {}", path.display(), e));
 
+    // root_url is absolute *relative to* the documentation directory. For `target/dir/crate_x/y`, it's `crate_x`.
     let root_url = Url::from_directory_path(root_dir).unwrap();
+    // base_url is the relative file path. For `target/dir/crate_x/y`, it's `crate_x/y`.
     let base_url = Url::from_file_path(path).unwrap();
-    let mut urls = HashSet::new();
-    parse_a_hrefs(&dom.document, &root_url, &base_url, &mut urls);
-    urls
+
+    parse_a_hrefs(&html, root_url, base_url)
 }
 
-/// Traverse the DOM of a parsed HTML element, extracting all URLs from <a href="xxx"> links.
-fn parse_a_hrefs(handle: &Handle, root_url: &Url, base_url: &Url, urls: &mut HashSet<Url>) {
-    let node = handle;
-    if let NodeData::Element {
-        ref name,
-        ref attrs,
-        ..
-    } = node.data
-    {
-        if &name.local == "a" {
-            if let Some(attr) = attrs
-                .borrow()
-                .iter()
-                .find(|attr| &attr.name.local == "href")
-            {
+/// This is a pure function, unlike `parse_html_file`, allowing it to be easily tested.
+fn parse_a_hrefs(html: &str, root_url: Url, base_url: Url) -> HashSet<Url> {
+    let mut urls = HashSet::new();
+    lol_html::rewrite_str(
+        html,
+        RewriteStrSettings {
+            element_content_handlers: vec![element!("a[href]", |el| {
+                let href = el.get_attribute("href").unwrap();
                 // base is the file path, unless path is absolute (starts with /)
-                let (base, href) = if attr.value.starts_with('/') {
+                let (base, href) = if href.starts_with('/') {
                     // Treat absolute paths as absolute with respect to the `root_url`, not with respect to the file system.
-                    let mut val = attr.value.clone();
-                    val.pop_front_char(); // remove the leading `/` and join on `root_url`
-                    (root_url, val)
+                    (&root_url, &href[1..])
                 } else {
-                    (base_url, attr.value.clone())
+                    (&base_url, href.as_str())
                 };
 
                 if let Ok(link) = base.join(&href) {
@@ -53,35 +41,20 @@ fn parse_a_hrefs(handle: &Handle, root_url: &Url, base_url: &Url, urls: &mut Has
                 } else {
                     debug!("unparsable link {:?}", href);
                 }
-            }
-        }
-    }
+                Ok(())
+            })],
+            ..RewriteStrSettings::default()
+        },
+    )
+    .expect("html rewriting failed");
 
-    for child in node.children.borrow().iter() {
-        parse_a_hrefs(&child, root_url, base_url, urls);
-    }
+    urls
 }
 
 #[cfg(test)]
 mod test {
-    use html5ever::parse_document;
-    use html5ever::{rcdom::RcDom, tendril::TendrilSink};
-    use std::collections::HashSet;
-    use url::Url;
-
     use super::parse_a_hrefs;
-
-    fn gather_urls(html: &str, root: &Url, url: &Url) -> HashSet<Url> {
-        let dom = parse_document(RcDom::default(), Default::default())
-            .from_utf8()
-            .read_from(&mut html.as_bytes())
-            .unwrap();
-
-        let mut urls = HashSet::new();
-        parse_a_hrefs(&dom.document, &root, &url, &mut urls);
-
-        return urls;
-    }
+    use url::Url;
 
     #[test]
     fn test_parse_a_hrefs() {
@@ -94,10 +67,10 @@ mod test {
             </body>
         </html>"#;
 
-        let urls = gather_urls(
+        let urls = parse_a_hrefs(
             html,
-            &Url::from_directory_path("/base").unwrap(),
-            &Url::from_file_path("/base/test.html").unwrap(),
+            Url::from_directory_path("/base").unwrap(),
+            Url::from_file_path("/base/test.html").unwrap(),
         );
 
         assert!(urls.contains(&Url::from_file_path("/base/a.html").unwrap()));
@@ -116,10 +89,10 @@ mod test {
             </body>
         </html>"#;
 
-        let urls = gather_urls(
+        let urls = parse_a_hrefs(
             html,
-            &Url::from_directory_path("/root").unwrap(),
-            &Url::from_file_path("/root/base/test.html").unwrap(),
+            Url::from_directory_path("/root").unwrap(),
+            Url::from_file_path("/root/base/test.html").unwrap(),
         );
 
         assert!(urls.contains(&Url::from_file_path("/root/base/a.html").unwrap()));
