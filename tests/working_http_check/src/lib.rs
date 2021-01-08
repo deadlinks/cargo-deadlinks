103cda0020cccf1f2f9775df08c20eb780fb3278
Joshua Nelson <jyn514@gmail.com>
2021-01-07 19:51:25 -0500

    Fallback to GET if HEAD isn't supported

--- tests/working_http_check/src/lib.rs
+++ tests/working_http_check/src/lib.rs
@@ -3,6 +3,7 @@
 /// Has something to do with [some website](http://example.com).
 ///
 /// Should also follow 308 redirects: <https://tinyurl.com/rnxcavf>
+/// If HEAD gives a 405 error, fall back to GET for <https://play.rust-lang.org/>
 pub fn foo() {}
 
 /// Bar function
